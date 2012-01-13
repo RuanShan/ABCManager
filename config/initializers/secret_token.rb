3a7419876b7a6a424f71ef1a09472db0c952ac79
zhang ze <david@david.home>
2012-01-13 20:02:49 +0800

    init project abcmanager

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ABCManager::Application.config.secret_token = '58adc0a4f371d897824cb5750270be919de2257ebc7e31beb58f7aafe683b31bfa2181d7f4fd6d56ce8e8b700e22981dc9242140185b4b4a7787ca46f1010ada'
