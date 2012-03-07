a9dfee9fc14154f2db994b2934c7ade1dfde792f
hui-green <zhangqh1984@sina.com>
2012-03-07 22:34:47 +0800

    Update db/migrate/20120305145524_shop.rb

--- db/migrate/20120305145524_shop.rb
+++ db/migrate/20120305145524_shop.rb
@@ -5,6 +5,9 @@ class Shop < ActiveRecord::Migration
       t.string :url
       t.string :owner,  :limit => 10*4,     :null => false, :default => "" # owner name
       t.string :owner_id # owner id card number
+      t.integer :province,  :limit => 2,  :null => false, :default => 0 
+      t.integer :city,  :limit => 3,  :null => false, :default => 0 
+      t.integer :district,  :limit => 4,  :null => false, :default => 0
       t.string :address,  :limit => 100*4,  :null => false, :default => "" # shop address detail
       #t.string :phone
       #t.string :email
